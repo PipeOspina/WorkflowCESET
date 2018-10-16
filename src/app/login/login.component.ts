abce54126fba668e7cb9de900f002200156c6255
Felipe Ospina <pipe.98.oh@gmail.com>
2018-10-16 14:16:10 -0500

    Trying to add email field

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @Component({
   selector: 'app-login',
